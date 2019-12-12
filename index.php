dc48afb43d9ed62ec5f5c433a58cf522c20d864e
DanEskildsen <DanEskildsen1234@users.noreply.github.com>
2019-12-12 16:58:14 +0100

    add all changes

--- index.php
+++ index.php
@@ -2,22 +2,31 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>OhShaker</title>
+    <title>Cocktails</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 </head>
 <body>
+    <div class="back-image">
+    <?php
+    include_once('components/header.php');
+    include_once('components/nav.php');
+    ?>
 
-<main>
-    <a class="btn add" href="create.php">+</a>
-    <section>
-        <a class="tile" href="single.php">
-            <img alt="Cocktail name">
-            <p>Cocktail name</p>
-        </a>
-    </section>
-</main>
+    <main>
+        <!-- <a class="btn add" href="create.php">+</a> -->
+        <section>
+            <a class="tile" href="single.php">
+                <img alt="Cocktail name">
+                <p>Cocktail name</p>
+            </a>
+        </section>
+    </main>
+    </div>
 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="assets/js/login.js"></script>
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,1 +1,48 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bartender login</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+</head>
+<body>
+    <?php
+    include_once('components/header.php');
+    include_once('components/nav.php');
+    ?>
+
+    <div class="back-image">
+
+    <main>
+        <!-- <span data-error class="error-box"></span> -->
+        <div class="select-style-1">
+            <select name="bar-select-drop" id="bar-select-drop">
+                <option value="0">Kea Bar</option>
+                <option value="1">Kea Bar</option>
+                <option value="2">Another bar</option>
+                <option value="3">A third bar</option>
+            </select>
+        </div>
+
+        <section class="form">
+            <p>Enter pin:</p>
+            <div id="pin-field">
+                <input type="password" class="pinput" maxlength="1">
+                <input type="password" class="pinput" maxlength="1">
+                <input type="password" class="pinput" maxlength="1">
+                <input type="password" class="pinput" maxlength="1">
+            </div>
+            <button class="btn btn-blue" data-login>Login</button>
+            <a href="manager-login.php">Administrative login</a>
+            <!-- <button class="btn" data-register>Register</button> -->
+        </section>
+    </main>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="assets/js/login.js"></script>
+</body>
+</html>
