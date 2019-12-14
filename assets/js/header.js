e544861eddea42a8cf9776de234a37342b74e0ee
Marijn Pool <15004290+ph00lt0@users.noreply.github.com>
2019-12-14 18:16:16 +0100

    add randomness to search

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,25 +1,34 @@
 function headerNav() {
     const openSearchButton = document.querySelector('[data-open-search]');
-    const SearchInput = document.querySelector('[data-search-input]');
+    const searchInput = document.querySelector('[data-search-input]');
     const backButton = document.querySelector('[data-back]');
     const searchResults = document.querySelector('[data-search-results]');
     const logo = document.querySelector('[data-logo]');
 
     openSearchButton.addEventListener('click', ()=> {
         toggleView();
-        SearchInput.value = "";
+        searchInput.value = "";
         searchResults.innerHTML = "";
     });
     backButton.addEventListener('click', ()=> {
-        if (SearchInput.classList.contains('hidden')) {
+        if (searchInput.classList.contains('hidden')) {
             window.history.back();
         } else {
             toggleView();
         }
     });
 
+    searchInput.addEventListener('keypress', () => {
+        document.querySelectorAll('[data-drinks] a').forEach( (cocktail) =>{
+            let change = Math.floor(Math.random() * 3) + 1;
+            if (change === 1) {
+                cocktail.classList.toggle('hidden');
+            }
+        });
+    });
+
     function toggleView() {
-        SearchInput.classList.toggle('hidden');
+        searchInput.classList.toggle('hidden');
         if (logo) {
             backButton.classList.toggle('hidden');
             logo.classList.toggle('hidden');
