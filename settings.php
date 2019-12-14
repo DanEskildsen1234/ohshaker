523c8f27bfa3169c1bbe7bacaee819a5cc78c6e5
Marijn Pool <15004290+ph00lt0@users.noreply.github.com>
2019-12-14 20:24:09 +0100

    add settings page

--- settings.php
+++ settings.php
@@ -1,1 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Settings</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+</head>
+<body>
 <?php
+include_once('components/header.php');
+include_once('components/nav.php');
+?>
+
+<main>
+    <section class="form settings">
+        <h2>Your information</h2>
+        <div>
+            <input value="John" placeholder="First name" class="" maxlength="1">
+            <input value="Doe" placeholder="Last name" class="" maxlength="1">
+        </div>
+        <div>
+            <input value="email@example.com" placeholder="Email" class="" maxlength="1">
+        </div>
+        <div>
+            <input value="Example Road 12" placeholder="Address" class="" maxlength="1">
+            <input value="2400" placeholder="Zip" class="" maxlength="1">
+            <input value="Copenhagen NV" disabled class="" maxlength="1">
+            <input value="Denmark" class="" maxlength="1">
+        </div>
+
+    </section>
+    <button class="btn" data-logout>Logout</button>
+</main>
+</body>
+</html>
