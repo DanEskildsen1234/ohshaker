f98ce8f60d966e29a48f2017b9ecdfd0ada8eeeb
ip3ly5 <ip3ly5@gmail.com>
2019-12-14 16:05:55 +0100

    made edit template

--- edit.php
+++ edit.php
@@ -1,1 +1,25 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Edit cocktail</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+</head>
+<body>
+    <?php
+        include_once('components/header.php');
+        include_once('components/nav.php');
+    ?>  
+
+    <!-- <div class="back-image"></div> -->
+    
+    <main>
+        <article>
+                <img class="cocktail-thumb-img singleImg" src="assets/img/cocktails/1-cropped-thumb.jpg" alt="Cocktail img"> 
+        </article>
+    </main>
+</body>
+</html>
